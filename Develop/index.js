83ab134271c3d8ff718d2e135efc27807db8f86f
Lina Zughaiyer <linazughaiyer@hotmail.com>
2021-02-01 19:46:31 -0500

    const operation

--- Develop/index.js
+++ Develop/index.js
@@ -4,3 +4,23 @@ const express = require("express");
 
 const router = express();
 const PORT = process.env.PORT || 8080;
+
+const parse = {
+    extended: true
+};
+
+const operation = [
+    express.urlencoded(parse),
+    express.json(),
+    express.static("public")
+
+];
+
+
+
+
+
+
+
+router.listen(PORT, () => console.log(`note taking app is listening on port ${PORT
+    }`));
